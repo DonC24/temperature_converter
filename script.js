5f4cc8006d37d2f51de1b141f59e7308eecc6a22
Donna <51435215+DonC24@users.noreply.github.com>
2019-06-26 17:20:38 +0800

    part 1 of temperature converter

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 console.log("hello script js");
 
+//part 1
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  //display( "WOW SOMETHING HAPPENED" );
+  var input = parseInt(currentInput); //change input from a string to a number
+  var FtoC = (input - 32) * (5/9); //calculate degrees F to degrees C
+  console.log(FtoC + " degrees C");
+
+  var FtoK = (((input - 32) * (5/9)) + 273.15); //calculate degrees F to Kelvin
+  console.log(FtoK + " Kelvin");
+  var output = FtoC + " degrees Celsius or " + FtoK + " Kelvin";
+
+  display(output);
 };
+
+/*var inputValid = function( currentInput ){
+
+  var parsedInput = parseInt( currentInput );
+
+  if( isNaN( parseInput ) ){
+    return false
+  }else{
+    return true;
+  }
+};
+
+if ()
+
+var inputHappened = function(currentInput){
+    console.log( currentInput );
+};*/
